cc68f9fb9aa81f51e6881643656df397bd1ad228
avinash Gunturu <avinashgunturu123@gmail.com>
2025-10-29 22:38:32 +0530

    addee =d front end domain route for fixing cors issue

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const app = express();
 // Middleware
 // Enable CORS
 app.use(cors({
-  origin: ['http://localhost:3000','http://localhost:5173','http://localhost:5174'], // Replace with your frontend URL
+  origin: ['http://localhost:3000','http://localhost:5173','http://localhost:5174',"https://pgfront.netlify.app/"], // Replace with your frontend URL
   methods: ['GET', 'POST','PUT','DELETE'], // Allowed methods
   credentials: true // Allow cookies if needed
 }));
